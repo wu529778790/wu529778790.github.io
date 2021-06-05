912f863061ffb16cb4390107c7b49d2e7905aec6
吴威 <529778790@qq.com>
2021-06-05 18:52:51 +0800

    增加百度meta验证

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -9,7 +9,7 @@ module.exports = [
       content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
     },
   ],
-  // ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证
+  ['meta', { name: 'baidu-site-verification', content: 'code-yywEP2q5jK' }], // 百度统计的站长验证
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
   // [
   //   'script',
