34eac574e1c54437988643df8a65c92199e2c9a2
吴威 <529778790@qq.com>
2021-06-05 19:34:07 +0800

    修改github链接

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/logo.png', // 导航栏logo
-  repo: 'wu529778790/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  repo: 'wu529778790/wu529778790.github.io', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
