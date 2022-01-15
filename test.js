1bfcd1b4f39891879685aa30427c20faf338b98e
wu529778790 <529778790@qq.com>
2022-01-15 13:50:07 +0800

    update

--- test.js
+++ test.js
@@ -6,3 +6,16 @@ Function.prototype.call = function (context, ...args) {
   delete context[fn];
   return res;
 };
+
+Function.prototype.bind = function (context, ...args) {
+  if (typeof context !== "function")
+    throw TypeError("context must be a function");
+  let that = this;
+  let rfn = function () {
+    that.call(context, ...args, arguments);
+  };
+  if (this.prototype) {
+    rfn.rptototype = Object.create(this.prototype);
+  }
+  return rfn;
+};
