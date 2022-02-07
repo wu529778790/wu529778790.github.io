3a7da84ae956977304520d5c3f001e8828f9687b
wu529778790 <529778790@qq.com>
2022-02-08 00:07:40 +0800

    update

--- test.js
+++ test.js
@@ -1,19 +1,29 @@
-console.log(4);
-const promise = new Promise((resolve) => {
-  console.log(2);
-  resolve();
-  console.log(6);
-});
-
-async function foo() {
-  console.log(1);
-  await promise;
-  console.log(3);
+function limitRequest(limit, requestes) {
+  let runCount = 0;
+  const request = () => {
+    if (!requestes || !requestes.length || runCount >= limit) {
+      return;
+    }
+    runCount++;
+    requestes
+      .shift()()
+      .then(() => {
+        runCount--;
+        request();
+      });
+  };
+  for (let i = 0; i < limit; i++) {
+    request();
+  }
 }
-
-foo();
-promise.then(() => {
-  console.log(5);
-});
-setTimeout(() => console.log(8));
-console.log(7);
+const timeout = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+const requestes = [
+  () => timeout(1000).then(() => console.log(11)),
+  () => timeout(500).then(() => console.log(22)),
+  () => timeout(300).then(() => console.log(33)),
+  () => timeout(400).then(() => console.log(44)),
+];
+limitRequest(2, requestes);
