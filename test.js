aaf92d12c45ef84967fb9ddd24f63474c9379047
wu529778790 <529778790@qq.com>
2022-02-09 18:33:18 +0800

    update

--- test.js
+++ test.js
@@ -1,29 +1,44 @@
-function limitRequest(limit, requestes) {
-  let runCount = 0;
-  const request = () => {
-    if (!requestes || !requestes.length || runCount >= limit) {
-      return;
-    }
-    runCount++;
-    requestes
-      .shift()()
-      .then(() => {
-        runCount--;
-        request();
-      });
-  };
-  for (let i = 0; i < limit; i++) {
-    request();
-  }
+// 为了演示方便，我们在此用fetchImage函数来模拟异步请求图片，返回成功提示
+function fetchImage(url) {
+  const timeCost = Math.random() * 3000;
+  return new Promise((resolve) => setTimeout(resolve, timeCost, "get:" + url));
 }
-const timeout = (time) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
-const requestes = [
-  () => timeout(1000).then(() => console.log(11)),
-  () => timeout(500).then(() => console.log(22)),
-  () => timeout(300).then(() => console.log(33)),
-  () => timeout(400).then(() => console.log(44)),
+
+// 待请求的图片
+const imageUrls = [
+  "pic_1.png",
+  "pic_2.png",
+  "pic_3.png",
+  "pic_4.png",
+  "pic_5.png",
+  "pic_6.png",
 ];
-limitRequest(2, requestes);
+
+/**
+ * @description 带并发限制的图片并发请求
+ * @param {Array} imageUrls 待请求的图片url列表
+ * @param {Object} limit 最大并发个数限制
+ * @return { Promise<Array> } resList
+ */
+function fetchImageWithLimit(imageUrls, limit = 2) {
+  let urls = [...imageUrls];
+  let rs = new Map();
+  function run() {
+    if (urls.length > 0) {
+      const url = urls.shift();
+      console.log(url, " [start at] ", new Date().getTime() % 10000);
+      return fetchImage(url).then((res) => {
+        console.log(url, " [end at] ", new Date().getTime() % 10000);
+        rs.set(url, res);
+        return run();
+      });
+    }
+  }
+  const promiseList = Array(Math.min(limit, imageUrls.length))
+    .fill(Promise.resolve())
+    .map((promise) => promise.then(run));
+  return Promise.all(promiseList).then(() =>
+    imageUrls.map((item) => rs.get(item))
+  );
+}
+fetchImageWithLimit(imageUrls).then((res) => console.log(res));
