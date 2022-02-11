2d8463a013ba9d404f58f89bb75c5fc627176d1f
wu529778790 <529778790@qq.com>
2022-02-11 15:30:32 +0800

    update

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-// 为了演示方便，我们在此用fetchImage函数来模拟异步请求图片，返回成功提示
-function fetchImage(url) {
-  const timeCost = Math.random() * 3000;
-  return new Promise((resolve) => setTimeout(resolve, timeCost, "get:" + url));
-}
-
 // 待请求的图片
 const imageUrls = [
   "pic_1.png",
@@ -13,32 +7,51 @@ const imageUrls = [
   "pic_5.png",
   "pic_6.png",
 ];
+// 模拟请求
+const request = (url) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, Math.random() * 3000, url);
+    // setTimeout(resolve, 1000, url);
+  });
+};
+const requestes = [];
+imageUrls.map((item) => {
+  requestes.push(request(item));
+});
 
-/**
- * @description 带并发限制的图片并发请求
- * @param {Array} imageUrls 待请求的图片url列表
- * @param {Object} limit 最大并发个数限制
- * @return { Promise<Array> } resList
- */
-function fetchImageWithLimit(imageUrls, limit = 2) {
-  let urls = [...imageUrls];
-  let rs = new Map();
-  function run() {
-    if (urls.length > 0) {
-      const url = urls.shift();
-      console.log(url, " [start at] ", new Date().getTime() % 10000);
-      return fetchImage(url).then((res) => {
-        console.log(url, " [end at] ", new Date().getTime() % 10000);
-        rs.set(url, res);
-        return run();
-      });
-    }
+const promiseWithLimit = (limit = 2, requestes) => {
+  const queue = [...requestes]
+  // let pool = []; //并发池
+
+  // 添加任务
+  function addTask(task) {
+    // pool.push(task);
+    task.then((res) => {
+      //请求结束后将该Promise任务从并发池中移除
+      // pool.splice(pool.indexOf(task), 1);
+      console.log(`${res}，当前并发数：${pool.length}`);
+      addTask(queue.shift())
+    });
   }
-  const promiseList = Array(Math.min(limit, imageUrls.length))
-    .fill(Promise.resolve())
-    .map((promise) => promise.then(run));
-  return Promise.all(promiseList).then(() =>
-    imageUrls.map((item) => rs.get(item))
-  );
-}
-fetchImageWithLimit(imageUrls).then((res) => console.log(res));
+
+  //每当并发池跑完一个任务，就再塞入一个任务
+  // function run(race) {
+  //   race.then(() => {
+  //     let task = requestes.shift();
+  //     if (task !== undefined) {
+  //       addTask(task);
+  //       run(Promise.race(pool));
+  //     }
+  //   });
+  // }
+  //先循环把并发池塞满
+  while (queue.length <= limit) {
+    console.log(queue)
+    addTask(queue.shift());
+  }
+  //利用Promise.race方法来获得并发池中某任务完成的信号
+  // let race = Promise.race(pool);
+  // run(race);
+};
+
+promiseWithLimit(2, requestes);
