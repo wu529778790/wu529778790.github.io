c310793d4a2b876ccbb89b5e1e4e8ecfac271afc
wu529778790 <529778790@qq.com>
2024-08-12 08:44:22 +0800

    fix：修改菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,34 @@
-import { blogPlugin } from '@vuepress/plugin-blog'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite'
+import { blogPlugin } from "@vuepress/plugin-blog";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: "en-US",
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: "VuePress",
+  description: "My first VuePress Site",
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+    logo: "https://vuejs.press/images/hero.png",
 
     navbar: [
-      '/',
+      "/",
+      // {
+      //   text: 'Article',
+      //   link: '/article/',
+      // },
+      // {
+      //   text: 'Category',
+      //   link: '/category/',
+      // },
+      // {
+      //   text: 'Tag',
+      //   link: '/tag/',
+      // },
       {
-        text: 'Article',
-        link: '/article/',
-      },
-      {
-        text: 'Category',
-        link: '/category/',
-      },
-      {
-        text: 'Tag',
-        link: '/tag/',
-      },
-      {
-        text: 'Timeline',
-        link: '/timeline/',
+        text: "Timeline",
+        link: "/timeline/",
       },
     ],
   }),
@@ -37,36 +37,36 @@ export default defineUserConfig({
     blogPlugin({
       // Only files under posts are articles
       filter: ({ filePathRelative }) =>
-        filePathRelative ? filePathRelative.startsWith('posts/') : false,
+        filePathRelative ? filePathRelative.startsWith("posts/") : false,
 
       // Getting article info
       getInfo: ({ frontmatter, title, data }) => ({
         title,
-        author: frontmatter.author || '',
+        author: frontmatter.author || "",
         date: frontmatter.date || null,
         category: frontmatter.category || [],
         tag: frontmatter.tag || [],
         excerpt:
           // Support manually set excerpt through frontmatter
-          typeof frontmatter.excerpt === 'string'
+          typeof frontmatter.excerpt === "string"
             ? frontmatter.excerpt
-            : data?.excerpt || '',
+            : data?.excerpt || "",
       }),
 
       // Generate excerpt for all pages excerpt those users choose to disable
       excerptFilter: ({ frontmatter }) =>
         !frontmatter.home &&
         frontmatter.excerpt !== false &&
-        typeof frontmatter.excerpt !== 'string',
+        typeof frontmatter.excerpt !== "string",
 
       category: [
         {
-          key: 'category',
+          key: "category",
           getter: (page) => page.frontmatter.category || [],
-          layout: 'Category',
-          itemLayout: 'Category',
+          layout: "Category",
+          itemLayout: "Category",
           frontmatter: () => ({
-            title: 'Categories',
+            title: "Categories",
             sidebar: false,
           }),
           itemFrontmatter: (name) => ({
@@ -75,12 +75,12 @@ export default defineUserConfig({
           }),
         },
         {
-          key: 'tag',
+          key: "tag",
           getter: (page) => page.frontmatter.tag || [],
-          layout: 'Tag',
-          itemLayout: 'Tag',
+          layout: "Tag",
+          itemLayout: "Tag",
           frontmatter: () => ({
-            title: 'Tags',
+            title: "Tags",
             sidebar: false,
           }),
           itemFrontmatter: (name) => ({
@@ -92,43 +92,44 @@ export default defineUserConfig({
 
       type: [
         {
-          key: 'article',
+          key: "article",
           // Remove archive articles
           filter: (page) => !page.frontmatter.archive,
-          layout: 'Article',
+          layout: "Article",
           frontmatter: () => ({
-            title: 'Articles',
+            title: "Articles",
             sidebar: false,
           }),
           // Sort pages with time and sticky
           sorter: (pageA, pageB) => {
             if (pageA.frontmatter.sticky && pageB.frontmatter.sticky)
-              return pageB.frontmatter.sticky - pageA.frontmatter.sticky
+              return pageB.frontmatter.sticky - pageA.frontmatter.sticky;
 
-            if (pageA.frontmatter.sticky && !pageB.frontmatter.sticky) return -1
+            if (pageA.frontmatter.sticky && !pageB.frontmatter.sticky)
+              return -1;
 
-            if (!pageA.frontmatter.sticky && pageB.frontmatter.sticky) return 1
+            if (!pageA.frontmatter.sticky && pageB.frontmatter.sticky) return 1;
 
-            if (!pageB.frontmatter.date) return 1
-            if (!pageA.frontmatter.date) return -1
+            if (!pageB.frontmatter.date) return 1;
+            if (!pageA.frontmatter.date) return -1;
 
             return (
               new Date(pageB.frontmatter.date).getTime() -
               new Date(pageA.frontmatter.date).getTime()
-            )
+            );
           },
         },
         {
-          key: 'timeline',
+          key: "timeline",
           // Only article with date should be added to timeline
           filter: (page) => page.frontmatter.date instanceof Date,
           // Sort pages with time
           sorter: (pageA, pageB) =>
             new Date(pageB.frontmatter.date).getTime() -
             new Date(pageA.frontmatter.date).getTime(),
-          layout: 'Timeline',
+          layout: "Timeline",
           frontmatter: () => ({
-            title: 'Timeline',
+            title: "Timeline",
             sidebar: false,
           }),
         },
@@ -138,4 +139,4 @@ export default defineUserConfig({
   ],
 
   bundler: viteBundler(),
-})
+});
