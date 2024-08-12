cd3af3f067f2e57a665d62afacc4b9475b1cea2b
wu529778790 <529778790@qq.com>
2024-08-12 09:58:07 +0800

    fix: update

--- docs/.vuepress/layouts/Timeline.vue
+++ docs/.vuepress/layouts/Timeline.vue
@@ -1,17 +1,16 @@
 <script setup>
-import { useBlogType } from '@vuepress/plugin-blog/client'
-import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
-import ArticleList from '../components/ArticleList.vue'
+import { useBlogType } from "@vuepress/plugin-blog/client";
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+import ArticleList from "../components/ArticleList.vue";
 
-const timelines = useBlogType('timeline')
+const timelines = useBlogType("timeline");
 </script>
 
 <template>
   <ParentLayout>
     <template #page>
       <main class="page">
-        <h1 class="timeline-title">Timeline</h1>
-
+        <h1 class="timeline-title">时间轴</h1>
         <ArticleList :items="timelines.items" is-timeline />
       </main>
     </template>
