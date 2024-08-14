69500553326f8f02aca0b293f38fee38d922da02
wuwei <wuwei@fadada.com>
2024-08-14 18:50:22 +0800

    fix: 修改配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,70 +31,23 @@ export default defineUserConfig({
 
   plugins: [
     blogPlugin({
-      // Only files under posts are articles
       filter: ({ filePathRelative }) =>
         filePathRelative ? filePathRelative.startsWith("posts/") : false,
-      // Getting article info
       getInfo: ({ frontmatter, title, data }) => ({
         title,
         author: frontmatter.author || "",
         date: frontmatter.date || null,
         category: frontmatter.category || [],
         tag: frontmatter.tag || [],
-        excerpt:
-          // Support manually set excerpt through frontmatter
-          typeof frontmatter.excerpt === "string"
-            ? frontmatter.excerpt
-            : data?.excerpt || "",
+        excerpt: data?.excerpt || "",
       }),
-      // Generate excerpt for all pages excerpt those users choose to disable
-      excerptFilter: ({ frontmatter }) =>
-        !frontmatter.home &&
-        frontmatter.excerpt !== false &&
-        typeof frontmatter.excerpt !== "string",
-      category: [
-        {
-          key: "category",
-          getter: (page) => page.frontmatter.category || [],
-          layout: "Category",
-          itemLayout: "Category",
-          frontmatter: () => ({
-            title: "Categories",
-            sidebar: false,
-          }),
-          itemFrontmatter: (name) => ({
-            title: `Category ${name}`,
-            sidebar: false,
-          }),
-        },
-        {
-          key: "tag",
-          getter: (page) => page.frontmatter.tag || [],
-          layout: "Tag",
-          itemLayout: "Tag",
-          frontmatter: () => ({
-            title: "Tags",
-            sidebar: false,
-          }),
-          itemFrontmatter: (name) => ({
-            title: `Tag ${name}`,
-            sidebar: false,
-          }),
-        },
-      ],
       type: [
         {
           key: "blog",
           filter: (page) => page.frontmatter.date instanceof Date,
-          // Sort pages with time
           sorter: (pageA, pageB) =>
             new Date(pageB.frontmatter.date).getTime() -
             new Date(pageA.frontmatter.date).getTime(),
-          layout: "Blog",
-          frontmatter: () => ({
-            title: "博客",
-            sidebar: false,
-          }),
         },
       ],
       hotReload: true,
