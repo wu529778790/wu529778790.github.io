4ab75461a68173a58847854b4281d910bf5508fe
wuwei <wuwei@fadada.com>
2024-08-19 10:13:53 +0800

    fix: 去掉生成cname文件，分开部署域名和github

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'blog.shenzjd.com' > CNAME
+# echo 'blog.shenzjd.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
