105f375d7f9b5ba82217dfbba000bdcd1957db9e
wuwei <wuwei@fadada.com>
2024-08-24 09:35:47 +0800

    fix: 修改菜单

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,61 +1,20 @@
 // nav
 module.exports = [
-  // {
-  //   text: "学习笔记",
-  //   link: "/note/",
-  // },
-  // {
-  //   text: "面试题",
-  //   link: "/interview/", //目录页链接，主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-  //   // items: [
-  //   //   // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-  //   //   {
-  //   //     // text: '前端文章',
-  //   //     items: [
-  //   //       { text: "HTML", link: "/pages/3cd9c5f306d2c/" },
-  //   //       { text: "CSS", link: "/pages/04403ba7faf73/" },
-  //   //       { text: "JavaScript", link: "/pages/6c3a148245a27/" },
-  //   //       { text: "Vue", link: "/pages/80abaefc76973/" },
-  //   //       { text: "面试真题", link: "/pages/0d28ab62c07b1/" }
-  //   //     ],
-  //   //   },
-  //   // ],
-  // },
-  // // {
-  // //   text: "索引",
-  // //   link: "/archives/",
-  // //   items: [
-  // //     { text: "分类", link: "/categories/" },
-  // //     // { text: "标签", link: "/tags/" },
-  // //   ],
-  // // },
-  // {
-  //   text: "收藏夹",
-  //   link: "/pages/beb6c0bd8a66cea6/",
-  //   items: [
-  //     { text: "网站", link: "/pages/beb6c0bd8a66cea6/" },
-  //     { text: "资源", link: "/pages/eee83a9211a70f9d/" },
-  //     { text: "Vue", link: "/pages/12df8ace52d493f6/" },
-  //   ],
-  // },
-  // {
-  //   text: "更多",
-  //   link: "/more/",
-  //   items: [
-  //     { text: "归档", link: "/archives/" },
-  //     // { text: "友链", link: "/friends/" },
-  //   ],
-  // },
   {
-    text: "网盘",
-    link: "https://alist.shenzjd.com",
-  },
-  {
-    text: "神卡",
-    link: "https://docs.qq.com/sheet/DV09NbXlvQW5QVHpp?tab=BB08J2",
-  },
-  {
-    text: "商城",
-    link: "http://91haoka.cn/gth/#/minishop?share_id=391093&shop_name=%25E7%25A5%259E%25E6%2597%258F%25E4%25B9%259D%25E5%25B8%259D",
+    text: "导航",
+    items: [
+      {
+        text: "神卡套餐",
+        link: "https://docs.qq.com/sheet/DV09NbXlvQW5QVHpp?tab=BB08J2",
+      },
+      {
+        text: "壁纸",
+        link: "https://wallpaper.shenzjd.com",
+      },
+      {
+        text: "网盘",
+        link: "https://alist.shenzjd.com",
+      },
+    ],
   },
 ];
